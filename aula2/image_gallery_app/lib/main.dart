0e7aed49ed31de5c12db9aa8c3939227bf40b463
“FelipeQueFez“ <felipequefez@gmail.com>
2024-08-01 12:01:54 +0100

    class 2

---
+++ aula2/image_gallery_app/lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+import 'image_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Image Gallery App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => ImageGallery(),
+        '/view': (context) => ImageView(),
+      },
+    );
+  }
+}
+
+class ImageGallery extends StatelessWidget {
+  final List<String> images = [
+    'assets/images/image1.jpg',
+    'assets/images/image2.jpg',
+    'assets/images/image3.png',
+    // Adicione mais caminhos de imagens
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Image Gallery'),
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, // Número de colunas
+        ),
+        itemCount: images.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.pushNamed(
+                context,
+                '/view',
+                arguments: images[index],
+              );
+            },
+            child: Card(
+              child: Image.asset(images[index]),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
